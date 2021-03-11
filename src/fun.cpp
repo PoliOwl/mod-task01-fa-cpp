b87f80ca1b31e05db504bb6379f833a3fb459f87
PoliOwl <polina_brzezinskaya@mail.ru>
2021-03-11 21:31:00 +0300

    [Polina Brzezinskaya] Task01

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,12 +1,75 @@
+const char *next_word(const char *point) {
+    while (*point == ' ') {
+        ++point;
+    }
+    return point;
+}
+
 unsigned int faStr1(const char *str)
 {
-    return 0;
+    if(*str == '\0') {
+        return 0;
+    }
+    const char* start = str;
+    const char *point = str;
+    unsigned int ans = 0;
+    point = next_word(point);
+    while (*point != '\0') {
+        unsigned int add = 1;
+        while (*point != ' ' && *point != '\0') {
+            if (add && *point - '0' > -1 && *point - '0' < 10) {
+                add = 0;
+            }
+            ++point;
+        }
+        ans+=add;
+        point = next_word(point);
+    }
+    return ans;
 }
 unsigned int faStr2(const char *str)
 {
-    return 0;
+    if(*str == '\0') {
+        return 0;
+    }
+    auto point = str;
+    unsigned int ans = 0;
+    while (*point != '\0') {
+        if (*point >= 'A' && *point <= 'Z') {
+            ++point;
+            unsigned int add = 1;
+            while (*point != ' ' && *point != '\0') {
+                if (*point < 'a' || *point > 'z') {
+                    add = 0;
+                    break;
+                }
+                ++point;
+            }
+            ans+=add;
+        }
+        while (*point != ' ') {
+            ++point;
+        }
+        point = next_word(point);
+    }
+    return ans;
 }
 unsigned int faStr3(const char *str)
 {
-    return 0;
+    if(*str == '\0') {
+        return 0;
+    }
+    auto point = str;
+    unsigned int allLen = 0;
+    unsigned int wordNum = 0;
+    point = next_word(point);
+    while (*point != '\0') {
+        ++wordNum;
+        while (*point != ' ' && *point != '\0') {
+            ++point;
+            ++allLen;
+        }
+        point = next_word(point);
+    }
+    return int(float(allLen)/float(wordNum)+0.5);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 #include "fun.h"
+#include <iostream>
 
 int main()
 {
+    std::cout << "str: \"  assw5 7  hfycn Alice Na56 KoKo     Help \"\n";
+    std::cout << "\tfaStr1 : " <<  faStr1("  assw5 7  hfycn Alice Na56 KoKo     Help ") << "\n";
+    std::cout << "\tfaStr2 : " <<  faStr2("  assw5 7  hfycn Alice Na56 KoKo     Help ") << "\n";
+    std::cout << "\tfaStr3 : " <<  faStr3("  assw5 7  hfycn Alice Na56 KoKo     Help ") << "\n";
     return 0;
 }
